8607697b5ae5332ffcd9a3021eba8d6fa7bb097a
Ryan ShrineFox <ShrineFox@users.noreply.github.com>
2020-09-14 06:46:11 -0400

    Extract AFS, convert to WAV
    Using PuyoTools & RADX. TODO: Repack AFS, extract/repack CSB

---
+++ SA2VoiceClipEditor/SA2VoicEClipEditorForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SA2VoiceClipEditor
+{
+    partial class SA2VoiceClipEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SA2VoiceClipEditorForm));
+            this.btn_ExtractAFS = new System.Windows.Forms.Button();
+            this.btn_RepackAFS = new System.Windows.Forms.Button();
+            this.btn_RepackCSB = new System.Windows.Forms.Button();
+            this.btn_ExtractCSB = new System.Windows.Forms.Button();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.chkBox_Convert = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_ExtractAFS
+            // 
+            this.btn_ExtractAFS.AllowDrop = true;
+            this.btn_ExtractAFS.Location = new System.Drawing.Point(12, 12);
+            this.btn_ExtractAFS.Name = "btn_ExtractAFS";
+            this.btn_ExtractAFS.Size = new System.Drawing.Size(175, 163);
+            this.btn_ExtractAFS.TabIndex = 0;
+            this.btn_ExtractAFS.Text = "Extract AFS";
+            this.btn_ExtractAFS.UseVisualStyleBackColor = true;
+            this.btn_ExtractAFS.DragDrop += new System.Windows.Forms.DragEventHandler(this.ExtractAFS_DragDrop);
+            this.btn_ExtractAFS.DragEnter += new System.Windows.Forms.DragEventHandler(this.ExtractAFS_DragEnter);
+            // 
+            // btn_RepackAFS
+            // 
+            this.btn_RepackAFS.AllowDrop = true;
+            this.btn_RepackAFS.Location = new System.Drawing.Point(12, 181);
+            this.btn_RepackAFS.Name = "btn_RepackAFS";
+            this.btn_RepackAFS.Size = new System.Drawing.Size(175, 44);
+            this.btn_RepackAFS.TabIndex = 1;
+            this.btn_RepackAFS.Text = "Repack AFS";
+            this.btn_RepackAFS.UseVisualStyleBackColor = true;
+            this.btn_RepackAFS.DragDrop += new System.Windows.Forms.DragEventHandler(this.RepackAFS_DragDrop);
+            this.btn_RepackAFS.DragEnter += new System.Windows.Forms.DragEventHandler(this.RepackAFS_DragEnter);
+            // 
+            // btn_RepackCSB
+            // 
+            this.btn_RepackCSB.AllowDrop = true;
+            this.btn_RepackCSB.Location = new System.Drawing.Point(193, 181);
+            this.btn_RepackCSB.Name = "btn_RepackCSB";
+            this.btn_RepackCSB.Size = new System.Drawing.Size(175, 44);
+            this.btn_RepackCSB.TabIndex = 3;
+            this.btn_RepackCSB.Text = "Repack CSB";
+            this.btn_RepackCSB.UseVisualStyleBackColor = true;
+            this.btn_RepackCSB.DragDrop += new System.Windows.Forms.DragEventHandler(this.RepackCSB_DragDrop);
+            this.btn_RepackCSB.DragEnter += new System.Windows.Forms.DragEventHandler(this.RepackCSB_DragEnter);
+            // 
+            // btn_ExtractCSB
+            // 
+            this.btn_ExtractCSB.AllowDrop = true;
+            this.btn_ExtractCSB.Location = new System.Drawing.Point(193, 12);
+            this.btn_ExtractCSB.Name = "btn_ExtractCSB";
+            this.btn_ExtractCSB.Size = new System.Drawing.Size(175, 163);
+            this.btn_ExtractCSB.TabIndex = 2;
+            this.btn_ExtractCSB.Text = "Extract CSB";
+            this.btn_ExtractCSB.UseVisualStyleBackColor = true;
+            this.btn_ExtractCSB.DragDrop += new System.Windows.Forms.DragEventHandler(this.ExtractCSB_DragDrop);
+            this.btn_ExtractCSB.DragEnter += new System.Windows.Forms.DragEventHandler(this.ExtractCSB_DragEnter);
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Location = new System.Drawing.Point(195, 240);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(0, 17);
+            this.lbl_Status.TabIndex = 4;
+            // 
+            // chkBox_Convert
+            // 
+            this.chkBox_Convert.AutoSize = true;
+            this.chkBox_Convert.Location = new System.Drawing.Point(12, 230);
+            this.chkBox_Convert.Name = "chkBox_Convert";
+            this.chkBox_Convert.Size = new System.Drawing.Size(137, 38);
+            this.chkBox_Convert.TabIndex = 5;
+            this.chkBox_Convert.Text = "Convert to WAV\r\n(can take awhile)";
+            this.chkBox_Convert.UseVisualStyleBackColor = true;
+            // 
+            // SA2VoiceClipEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 268);
+            this.Controls.Add(this.chkBox_Convert);
+            this.Controls.Add(this.lbl_Status);
+            this.Controls.Add(this.btn_RepackCSB);
+            this.Controls.Add(this.btn_ExtractCSB);
+            this.Controls.Add(this.btn_RepackAFS);
+            this.Controls.Add(this.btn_ExtractAFS);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SA2VoiceClipEditorForm";
+            this.Text = "SA2 Voice Clip Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ExtractAFS;
+        private System.Windows.Forms.Button btn_RepackAFS;
+        private System.Windows.Forms.Button btn_RepackCSB;
+        private System.Windows.Forms.Button btn_ExtractCSB;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.CheckBox chkBox_Convert;
+    }
+}
+
---
+++ SA2VoiceClipEditor/SA2VoicEClipEditorForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PuyoTools.Modules.Archive;
+using PuyoTools.Formats.Archives;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SA2VoiceClipEditor
+{
+    public partial class SA2VoiceClipEditorForm : Form
+    {
+        string afsPath = "";
+        string csbPath = "";
+        string extractedFilesPath = "";
+
+        public SA2VoiceClipEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExtractAFS_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            var settings = new PuyoTools.GUI.ArchiveExtractor.Settings();
+            if (File.Exists(data[0]) && Path.GetExtension(data[0]).ToLower() == ".afs")
+            {
+                lbl_Status.Text = "Extracting AFS...";
+                Thread.Sleep(100);
+                //Extract AFS
+                afsPath = data[0];
+                extractedFilesPath = Path.Combine(Path.Combine(Path.Combine(Path.GetDirectoryName(afsPath), "Extracted Files")), Path.GetFileNameWithoutExtension(afsPath));
+                List<string> files = new List<string>();
+                files.Add(afsPath);
+                PuyoTools.GUI.ArchiveExtractor.Run(settings, files);
+
+                //rename extracted files to include .ahx extension
+                string[] lines = File.ReadAllLines("event_adx.txt");
+                foreach (var file in Directory.GetFiles(extractedFilesPath, "*", SearchOption.AllDirectories))
+                {
+                    string newPath = file;
+                    if (Path.GetFileName(afsPath).StartsWith("event_adx") && Path.GetFileName(newPath) != "AFS")
+                    {
+                        //Change path if numbered event_adx is being extracted
+                        int lineNumber = Convert.ToInt32(Path.GetFileNameWithoutExtension(file));
+                        string characterName = lines[lineNumber].Split('\t')[0];
+                        newPath = file.Replace(Path.GetFileNameWithoutExtension(afsPath) + "\\", "").Replace("Extracted Files", $"Extracted Files\\{Path.GetFileNameWithoutExtension(afsPath)}_{characterName}");
+                        if (!Directory.Exists(Path.GetDirectoryName(newPath)))
+                            Directory.CreateDirectory(Path.GetDirectoryName(newPath));
+                    }
+                    //Include .ahx in new path name and move
+                    using (WaitForFile(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None)) { };
+                    if (Path.GetExtension(file) != ".ahx" && !newPath.Contains("AFS"))
+                        newPath = newPath + ".ahx";
+                    else if (newPath.Contains("AFS") && Path.GetFileName(afsPath).StartsWith("event_adx"))
+                        newPath = newPath.Replace(Path.GetFileNameWithoutExtension(afsPath) + "\\", "").Replace("Extracted Files", $"Extracted Files\\{Path.GetFileNameWithoutExtension(afsPath)}_Pilot").Replace("AFS","0000.ahx");
+                    else
+                        newPath = newPath.Replace("AFS", "0000.ahx");
+
+                    File.Move(file, newPath);
+                    using (WaitForFile(newPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None)) { };
+                }
+                //Delete empty event_adx extracted AFS folder
+                lbl_Status.Text = "Done Extracting AFS";
+                if (Path.GetFileName(afsPath).StartsWith("event_adx"))
+                    Directory.Delete(extractedFilesPath, true);
+
+                //Convert all AHX files to WAV
+                if (chkBox_Convert.Checked)
+                {
+                    lbl_Status.Text = "Converting AHX to WAV...";
+                    foreach (var ahx in Directory.GetFiles(Path.GetDirectoryName(extractedFilesPath), "*.ahx", SearchOption.AllDirectories))
+                    {
+                        RunCMD($"radx_decode.exe \"{ahx}\" \"{ahx.Replace(".ahx", ".wav")}\"");
+                        using (WaitForFile(ahx.Replace(".ahx", ".wav"), FileMode.Open, FileAccess.ReadWrite, FileShare.None)) { };
+                        foreach (var process in Process.GetProcessesByName("cmd"))
+                            process.Kill();
+                        File.Delete(ahx);
+                    }
+                    lbl_Status.Text = "Done Converting AHX";
+                }
+            }
+        }
+
+        private void Rename_Files_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("cmd"))
+                process.Kill();
+        }
+
+        private void RepackAFS_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ExtractCSB_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (File.Exists(data[0]) && Path.GetExtension(data[0]).ToLower() == ".csb")
+            {
+                csbPath = data[0];
+                extractedFilesPath = Path.Combine(Path.GetDirectoryName(csbPath), Path.GetFileNameWithoutExtension(csbPath));
+                RunCMD($"CsbEditor.exe \"{csbPath}\"");
+                Thread.Sleep(1000);
+                foreach (var file in Directory.GetFiles(extractedFilesPath, "*.adx", SearchOption.AllDirectories))
+                {
+                    if (new FileInfo(file).Length > 60)
+                        File.Move(file, Path.Combine(extractedFilesPath, Path.GetFileName(Path.GetDirectoryName(file)) + ".adx"));
+                }
+                Thread.Sleep(1000);
+                RecursiveDelete(new DirectoryInfo(extractedFilesPath));
+                lbl_Status.Text = "Done extracting CSB";
+            }
+        }
+
+        private void RunCMD(string args, bool hidden = true)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "cmd";
+            start.Arguments = $"/K {args}";
+            start.UseShellExecute = true;
+            start.RedirectStandardOutput = false;
+            if (hidden)
+                start.WindowStyle = ProcessWindowStyle.Hidden;
+            using (Process process = Process.Start(start))
+            {
+
+            }
+        }
+
+        private void RepackCSB_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ExtractAFS_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void RepackAFS_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void ExtractCSB_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void RepackCSB_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        public static void RecursiveDelete(DirectoryInfo baseDir)
+        {
+            if (!baseDir.Exists)
+                return;
+
+            foreach (var dir in baseDir.EnumerateDirectories())
+            {
+                RecursiveDelete(dir);
+                dir.Delete(true);
+            }
+            //baseDir.Delete(true);
+        }
+
+        FileStream WaitForFile(string fullPath, FileMode mode, FileAccess access, FileShare share)
+        {
+            for (int numTries = 0; numTries < 10; numTries++)
+            {
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(fullPath, mode, access, share);
+                    return fs;
+                }
+                catch (IOException)
+                {
+                    if (fs != null)
+                    {
+                        fs.Dispose();
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+
+            return null;
+        }
+    }
+}
